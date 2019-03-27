c01fe1aaa931ddb93c439a17bf7ca8bdd75babf0
Yana Mancevich <yaninamantsevich@mail.ru>
2019-03-27 23:33:51 +0200

    add missing translations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-    I18n.default_locale != I18n.locale ? I18n.locale : {}
+    I18n.default_locale != I18n.locale ? { lang: I18n.locale } : {}
   end
 
   private
