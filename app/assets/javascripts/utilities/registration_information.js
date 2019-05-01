8ad109bd039b45e15a3999ccc0f28504c86fa79f
Yana Mancevich <yaninamantsevich@mail.ru>
2019-05-02 00:05:03 +0200

    add javascript to project

---
+++ app/assets/javascripts/utilities/registration_information.js
@@ -0,0 +1,26 @@
+document.addEventListener('turbolinks:load', function() {
+  password.addEventListener('click', checkPassword)
+  password_confirmation.addEventListener('click', checkPassword)
+})
+
+function checkPassword() {
+
+  var password = document.getElementById('password');
+  var password_confirmation = document.getElementById('password_confirmation');
+
+  if(password_confirmation.value.length == 0 ) {
+    password_confirmation.style.backgroundColor = 'white';
+  }
+
+  if(password.value == password_confirmation.value && password.value.length >= 6) {
+    
+    password_confirmation.style.backgroundColor = 'green';
+    message.style.color = 'green';
+  }
+
+  if(password.value != password_confirmation.value && password.value.length >= 6) {
+    password_confirmation.style.backgroundColor = 'red';
+    message.style.color = 'red';
+  }
+
+}
