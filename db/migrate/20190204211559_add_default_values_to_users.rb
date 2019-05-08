cae672a22dd33e849b3ff9a29ff19c71899cf4ab
Yana Mancevich <yaninamantsevich@mail.ru>
2019-05-08 21:09:56 +0200

    fix migrations

--- db/migrate/20190204211559_add_default_values_to_users.rb
+++ db/migrate/20190204211559_add_default_values_to_users.rb
@@ -1,8 +1,8 @@
 class AddDefaultValuesToUsers < ActiveRecord::Migration[5.1]
   def change
     change_column_default(:users, :name, from: nil, to: "")
-    change_column_default(:users, :created_at, from: nil, to: 01-01-2019)
-    change_column_default(:users, :updated_at, from: nil, to: 01-01-2019)
+   # change_column_default(:users, :created_at, from: nil, to: 01-01-2019)
+   # change_column_default(:users, :updated_at, from: nil, to: 01-01-2019)
     change_column_default(:users, :admin, from: nil, to: false)
   end
 end
